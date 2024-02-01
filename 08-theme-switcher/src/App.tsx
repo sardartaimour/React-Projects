a11dd1bb1b5666a49935bd1ac5cf01212a0d4b2f
taimour-niche <94434448+taimour-niche@users.noreply.github.com>
2024-02-02 00:10:18 +0500

    theme context

--- 08-theme-switcher/src/App.tsx
+++ 08-theme-switcher/src/App.tsx
@@ -1,12 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { ThemeContextProvider } from "./Context/Theme";
+import ThemeButton from "./Components/ThemeButton";
+import Card from "./Components/Card";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [themeMode, setThemeMode] = useState("light");
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  useEffect(() => {
+    const html: any = document.querySelector("html");
+    html.classList.remove("dark", "light");
+    html.classList.add(themeMode);
+  }, [themeMode]);
 
   return (
-    <>
-      <h1>Context Api</h1>
-    </>
+    <ThemeContextProvider value={{ themeMode, darkTheme, lightTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeButton />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeContextProvider>
   );
 }
 
